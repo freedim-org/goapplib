682e512c226028ce7b9c1a4c4135146024230d5f
Freegram Developer <github@freegram.email>
2023-11-27 17:52:42 +0800

    test freeport

--- tools/tools.go
+++ tools/tools.go
@@ -1,17 +1,16 @@
 package tools
 
-import "net"
-
 func FreePort() int32 {
-	// 获取空闲端口
-	var a *net.TCPAddr
-	var err error
-	if a, err = net.ResolveTCPAddr("tcp", "localhost:0"); err == nil {
-		var l *net.TCPListener
-		if l, err = net.ListenTCP("tcp", a); err == nil {
-			defer l.Close()
-			return int32(l.Addr().(*net.TCPAddr).Port)
-		}
-	}
-	panic(err)
+	//// 获取空闲端口
+	//var a *net.TCPAddr
+	//var err error
+	//if a, err = net.ResolveTCPAddr("tcp", "localhost:0"); err == nil {
+	//	var l *net.TCPListener
+	//	if l, err = net.ListenTCP("tcp", a); err == nil {
+	//		defer l.Close()
+	//		return int32(l.Addr().(*net.TCPAddr).Port)
+	//	}
+	//}
+	//panic(err)
+	return 12345
 }
