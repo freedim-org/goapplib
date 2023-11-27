d64026d39938dfcc3dd3211386da4ce2bc6e76d1
Freegram Developer <github@freegram.email>
2023-11-27 18:19:08 +0800

    feat: version

---
+++ cmd/version/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/freedim-org/goapplib"
+)
+
+func main() {
+	fmt.Print(goapplib.Version)
+}
---
+++ version.go
@@ -0,0 +1,5 @@
+package goapplib
+
+const (
+	Version = "v0.0.1.rc1"
+)
